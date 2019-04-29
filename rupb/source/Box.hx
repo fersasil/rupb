ab47213493e3f1d6f54a3340b57bdc56d43d8626
Fersasil <guilhermefersasil@gmail.com>
2019-04-29 12:17:38 -0300

    Tamanho do sprite da boc atualizado

--- rupb/source/Box.hx
+++ rupb/source/Box.hx
@@ -4,8 +4,8 @@ import flixel.FlxSprite;
 
 class Box extends FlxSprite{
     public function new(X:Float = 0, Y:Float = 0){
-        super(X, Y);
-        loadGraphic(AssetPaths.box__png, false, 7, 9);
+        super(X + 2, Y + 2);
+        loadGraphic(AssetPaths.box12x12__png, false, 12, 12);
         this.immovable = true;
     }
 }
