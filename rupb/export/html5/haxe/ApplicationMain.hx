9cf78f268fe2c20642ec363bfcea162b16d75299
Fersasil <gfersasil@gmail.com>
2020-01-11 20:10:38 -0300

    Last pull request undone

--- rupb/export/html5/haxe/ApplicationMain.hx
+++ rupb/export/html5/haxe/ApplicationMain.hx
@@ -32,7 +32,7 @@ class ApplicationMain
 
 		ManifestResources.init(config);
 
-		app.meta["build"] = "36";
+		app.meta["build"] = "40";
 		app.meta["company"] = "HaxeFlixel";
 		app.meta["file"] = "rupb";
 		app.meta["name"] = "rupb";
